4d8ea892896504bb3a9781e4e06f9f00c7fbef5d
VT008 <m2brodfu@vt.edu>
2020-12-10 14:41:57 -0500

    removed spaces from msg

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func tcpclient(ch <-chan coinbasepro.Message) {
 			//Skip blank price messages
 			if v.Price != "" {
 				tm := strconv.Itoa(int(v.Time.Time().Unix()))
-				jsonMsg := fmt.Sprintf("[{market:%s,price:%s,size:%s,side: %s, bestBid:%s, bestAsk:%s, tm:%s}]", v.ProductID, v.Price, v.LastSize, v.Side, v.BestBid, v.BestAsk, tm)
+				jsonMsg := fmt.Sprintf("[{market:%s,price:%s,size:%s,side:%s,bestBid:%s,bestAsk:%s,tm:%s}]", v.ProductID, v.Price, v.LastSize, v.Side, v.BestBid, v.BestAsk, tm)
 				// send to socket
 				fmt.Fprintf(conn, jsonMsg+"\n")
 				// listen for reply
